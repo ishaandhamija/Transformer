869b459bc92dc43bdeadded19e5aab5e6c3d7035
Ishaan Dhamija <ishaandhamija@Ishaans-MacBook-Air.local>
2017-07-31 14:12:00 +0530

    initial commit

---
+++ app/src/main/java/com/example/ishaandhamija/transformer/Activities/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.ishaandhamija.transformer.Activities;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.ishaandhamija.transformer.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final Integer REQ_CODE = 1001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int receiveSmsPerm = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
+        int readSmsPerm = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS);
+
+        if ((receiveSmsPerm != PackageManager.PERMISSION_GRANTED) || (readSmsPerm != PackageManager.PERMISSION_GRANTED)){
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.READ_CONTACTS,
+                    Manifest.permission.READ_PHONE_STATE
+            }, REQ_CODE);
+        }
+        else {
+
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == REQ_CODE) {
+            for (int result : grantResults) {
+                if (result == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(this, "Permission Not Given", Toast.LENGTH_SHORT).show();
+                    finish();
+                    return;
+                }
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+}
---
+++ app/src/main/java/com/example/ishaandhamija/transformer/BroadcastReceiver/IncomingSms.java
@@ -0,0 +1,53 @@
+package com.example.ishaandhamija.transformer.BroadcastReceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by ishaandhamija on 31/07/17.
+ */
+
+public class IncomingSms extends BroadcastReceiver {
+
+    final SmsManager sms = SmsManager.getDefault();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final Bundle bundle = intent.getExtras();
+
+        try {
+
+            if (bundle != null) {
+
+                final Object[] pdusObj = (Object[]) bundle.get("pdus");
+
+                for (int i = 0; i < pdusObj.length; i++) {
+
+                    SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+
+                    String phoneNumber = currentMessage.getDisplayOriginatingAddress();
+                    String senderNum = phoneNumber;
+                    String message = currentMessage.getDisplayMessageBody();
+
+                    Log.d("SmsReceiver", "senderNum: "+ senderNum + "; message: " + message);
+
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context,
+                            "senderNum: "+ senderNum + ", message: " + message, duration);
+                    toast.show();
+
+                }
+            }
+
+        } catch (Exception e) {
+            Log.e("SmsReceiver", "Exception smsReceiver" +e);
+
+        }
+    }
+}
